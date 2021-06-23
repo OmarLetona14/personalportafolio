fc1e090b5f3bad959ef0441fa5447d70991317df
OmarLetona14 <omarletonaf@gmail.com>
2021-06-23 03:19:29 +0000

    Deploying to gh-pages from  @ 79f8c660f9e53a9c23a4767ed337f2731406e5a4 🚀

---
+++ precache-manifest.c7026a4b421b2f8b3a647ec21b964afc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bddab309b9f9888cded521d164c64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63db71216ec1506b379e",
+    "url": "/static/css/3.37399f2e.chunk.css"
+  },
+  {
+    "revision": "acf8f630ae3e4d561d6b",
+    "url": "/static/css/4.1581f9d0.chunk.css"
+  },
+  {
+    "revision": "063f5bad636dc272f49b",
+    "url": "/static/css/main.27b50a89.chunk.css"
+  },
+  {
+    "revision": "718c8ae10c956cfa52c5",
+    "url": "/static/js/2.c996a98a.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.c996a98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63db71216ec1506b379e",
+    "url": "/static/js/3.588513cb.chunk.js"
+  },
+  {
+    "revision": "acf8f630ae3e4d561d6b",
+    "url": "/static/js/4.ff613e49.chunk.js"
+  },
+  {
+    "revision": "063f5bad636dc272f49b",
+    "url": "/static/js/main.f080801f.chunk.js"
+  },
+  {
+    "revision": "63c4076074178ac0e466",
+    "url": "/static/js/runtime-main.fc37a80e.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "11673f56b9ed74eda4a01eb65d5e5669",
+    "url": "/static/media/allied.11673f56.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "d20c68c1096df8b3f4b5b4d1fd080180",
+    "url": "/static/media/kinal_logo-removebg.d20c68c1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
